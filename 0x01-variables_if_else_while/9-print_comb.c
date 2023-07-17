5b13c25f633ff402150f6b7a2449d623db56a8bc
root <abdallahmomen122@gmail.com>
2023-07-16 23:40:54 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
  * Description: print 0, 1, - 9
  *
- * return: always 0 (success)
+ * Return: Always 0 (success)
 */
 
 int main(void)
