29e900a1df1765abc2e755bc5bafa82e2d89faf4
root <abdallahmomen122@gmail.com>
2023-07-16 23:22:40 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,38 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - entry point
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
  *
- * Description: print 0, 1, - 9
- *
- * return: always 0 (success)
-*/
-
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int digit1 = 0, digit2;
+	int digit1, digit2, digit3;
 
-	while (digit2 <= 9)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		digit2 = 0;
-
-		while (digit2 <= 9)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			if (digit1 != digit2 && digit1 < digit2)
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-				putchar(digit1 + 48);
-				putchar(digit2 + 48);
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
 
-				if (digi1 + digit2 != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
 			}
-			digit2++;
 		}
-		digit1++;
 	}
+
 	putchar('\n');
 
 	return (0);
